a9bd55cb80a5012918c629bcf73f8d1c32fee483
dape82 <daniel@pecar.de>
2026-02-16 19:34:38 +0000

    fix

--- custom_components/homecontrol/__init__.py
+++ custom_components/homecontrol/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
-from HomeControlIntegration.custom_components.homecontrol.const import ERROR_VIEW_REGISTRATION_FAILED
+from .const import ERROR_VIEW_REGISTRATION_FAILED
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryError
